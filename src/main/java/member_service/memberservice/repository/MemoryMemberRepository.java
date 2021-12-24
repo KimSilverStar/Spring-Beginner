dc4e017e76034db7111fdca0ae090b37e7f14164
unknown <dmstjd365@kw.ac.kr>
2021-12-24 15:12:21 +0900

    회원 도메인 Member, 회원 리포지토리 MemberRepository, MemoryMemberRepository 추가

---
+++ src/main/java/member_service/memberservice/repository/MemoryMemberRepository.java
@@ -0,0 +1,38 @@
+package member_service.memberservice.repository;
+
+import member_service.memberservice.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+	// !실무에서는 static 멤버 변수(클래스 변수) 사용 시, 동시성 문제 고려!
+	private static Map<Long, Member> store = new HashMap<>();
+	private static long sequence = 0L;
+
+	@Override
+	public Member save(Member member) {
+		member.setId(++sequence);
+		store.put(member.getId(), member);
+		return member;
+	}
+
+	@Override
+	public Optional<Member> findById(Long id) {
+		return Optional.ofNullable(store.get(id));
+		// store HashMap 에 해당 id 의 Member 가 없는 경우, NULL 발생
+		// => Optional 로 감싸서 반환
+	}
+
+	@Override
+	public Optional<Member> findByName(String name) {
+		return store.values().stream()
+				.filter(member -> member.getName().equals(name))
+				.findAny();
+		// filter().findAny() => 조건 만족하는 요소 1개라도 찾으면 반환
+	}
+
+	@Override
+	public List<Member> findAll() {
+		return new ArrayList<>(store.values());
+	}
+}
