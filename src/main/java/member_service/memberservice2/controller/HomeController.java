de75cce59e3c987532314af8355dff18a255b5f0
unknown <dmstjd365@kw.ac.kr>
2021-12-25 15:10:21 +0900

    회원 웹 기능 - 홈 화면 추가: HomeController, home.html

---
+++ src/main/java/member_service/memberservice2/controller/HomeController.java
@@ -0,0 +1,13 @@
+package member_service.memberservice2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String home() {
+		return "home";
+		// "localhost:8080/" => home.html
+	}
+}
